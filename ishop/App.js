198ae3931180e36ce106705df69c9272ad003445
AndreyKlimenchenko <klimenchenkoandrey@gmail.com>
2024-02-10 12:06:56 +0300

    added shop table

---
+++ ishop/App.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ShopBlock from './components/Shop';
+
+import products from './products.json';
+
+ReactDOM.render(
+  <ShopBlock 
+    products={products}
+  />
+  , document.getElementById('container') 
+);
