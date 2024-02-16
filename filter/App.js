65f00ee4a7a17f77a21e5fbbb6f1bd9f18353269
AndreyKlimenchenko <klimenchenkoandrey@gmail.com>
2024-02-16 17:40:07 +0300

    fixed code

---
+++ filter/App.js
@@ -0,0 +1,6 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+import Filter from "./components/Filter";
+
+ReactDOM.render(<Filter />, document.getElementById("container"));
---
+++ filter/components/Filter.js
@@ -0,0 +1,16 @@
+﻿import React from "react";
+import PropTypes from "prop-types";
+
+import "./Filter.css";
+
+class Filter extends React.Component {
+  static propTypes = {};
+
+  state = {};
+
+  render() {
+    return <div className="filter-container">Hello</div>;
+  }
+}
+
+export default Filter;
