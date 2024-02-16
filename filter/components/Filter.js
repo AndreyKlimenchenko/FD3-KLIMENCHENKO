aea31e8286ccbb50ce64edd4aaf00894b04f1c0e
AndreyKlimenchenko <klimenchenkoandrey@gmail.com>
2024-02-16 18:15:53 +0300

    added filter

--- filter/components/Filter.js
+++ filter/components/Filter.js
@@ -7,18 +7,33 @@ class Filter extends React.Component {
   static propTypes = {
     strings: PropTypes.arrayOf(PropTypes.string),
   };
-  state = {};
+
+  state = {
+    strings: this.props.strings,
+  };
+
+  handleChange = (value) => {
+    const newStrings = value
+      ? this.props.strings.filter((item) => {
+          return item.includes(value);
+        })
+      : this.props.strings;
+
+    this.setState({
+      strings: newStrings,
+    });
+  };
 
   render() {
     return (
       <div className="filter-container">
         <div className="select-container">
-          <input type="checkbox" name="order"/>
-          <input/>
+          <input type="checkbox" name="order" />
+          <input onChange={(event) => this.handleChange(event.target.value)} />
           <button>сброс</button>
         </div>
         <div className="strings-container">
-          {this.props.strings.map((item) => {
+          {this.state.strings.map((item) => {
             return <div key={item}>{item}</div>;
           })}
         </div>
