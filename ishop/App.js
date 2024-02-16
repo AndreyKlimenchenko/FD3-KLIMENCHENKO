1dda9308e1368d1ccf563e1b4f0d8eebba860d06
AndreyKlimenchenko <klimenchenkoandrey@gmail.com>
2024-02-16 12:04:36 +0300

    fixed

---
+++ ishop/App.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ShopBlock from './components/Shop';
+
+import products from './products.json';
+
+ReactDOM.render(
+  <ShopBlock 
+    products={products}
+    shopName="каталог объективов"
+  />
+  , document.getElementById('container') 
+);
---
+++ ishop2/components/Shop.js
@@ -0,0 +1,66 @@
+﻿import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import "./Shop.css";
+import Product from "./Product";
+
+const Shop = (props) => {
+  const [selectedProductID, setSelectedProductID] = useState('');
+  const [products, setProducts] = useState(props.products);
+
+  const handleSelect = (id) => {
+    if (id === selectedProductID) {
+      setSelectedProductID('');
+    } else {
+      setSelectedProductID(id)
+    }
+  };
+
+  const handleDelete = (id) => {
+    const response = confirm('Уверены?');
+    if (response) {
+      setProducts(prevState => {
+        const newProducts = prevState.filter((item) => item.id !== id)
+        return newProducts;
+      })
+    }
+  };
+
+  return (
+    <div className="shop-container">
+      <h1>{props.shopName}</h1>
+      <table className="shop-table">
+        <tbody>
+          <tr>
+            <th></th>
+            <th>имя</th>
+            <th>цена</th>
+            <th>остаток</th>
+            <th></th>
+          </tr>
+          {products.map((item) => {
+
+            const isSelected = selectedProductID === item.id;
+
+            return <Product product={item} key={item.id} isSelected={isSelected} handleSelect={handleSelect} handleDelete={handleDelete}/>;
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+Shop.propTypes = {
+  shopName: PropTypes.string,
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string,
+      count: PropTypes.number,
+      url: PropTypes.string,
+      price: PropTypes.number,
+    })
+  ),
+};
+
+export default Shop;
