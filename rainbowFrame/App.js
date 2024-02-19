ee5a3f8b730b99ea5ed8f4ffbdb65dd2b7a2a5c0
AndreyKlimenchenko <klimenchenkoandrey@gmail.com>
2024-02-19 15:28:13 +0300

    added files

---
+++ rainbowFrame/App.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowBlock from './components/Rainbow';
+
+ReactDOM.render(
+  <RainbowBlock 
+    products={products}
+    shopName="каталог объективов"
+  />
+  , document.getElementById('container') 
+);
---
+++ rainbowFrame/components/Rainbow.js
@@ -0,0 +1,18 @@
+﻿import React from "react";
+
+import "./Rainbow.css";
+
+class Rainbow extends React.Component {
+  static propTypes = {
+  };
+  
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Rainbow;
