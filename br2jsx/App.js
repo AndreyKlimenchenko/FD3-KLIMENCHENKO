7946b60a7ff45bf9234ff555dec0d549ccc0cc23
AndreyKlimenchenko <klimenchenkoandrey@gmail.com>
2024-02-20 13:55:45 +0300

    added func

---
+++ br2jsx/App.js
@@ -0,0 +1,10 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+import Br2jsxBlock from './components/Br2jsxBlock';
+
+
+ReactDOM.render(
+    <Br2jsxBlock/>,
+    document.getElementById('container')
+);
---
+++ br2jsx/components/Br2jsxComponent.js
@@ -0,0 +1,30 @@
+﻿import React from "react";
+import PropTypes from "prop-types";
+
+import "./Br2jsxComponent.css";
+
+class BR2JSX extends React.Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+  };
+
+  render() {
+
+    let strArr = this.props.text.split(/(?:<br>|<br\/>|<br\s+\/>)+/);
+    
+    let arr = [];
+
+    for (let i = 0; i <= strArr.length - 1; i++) {
+      if (i == strArr.length - 1) {
+        arr.push(strArr[i]);
+      } else {
+        let e = <br key={i} />;
+        arr.push(strArr[i], e);
+      }
+    }
+    
+    return <div className="container">{arr}</div>;
+  }
+}
+
+export default BR2JSX;
