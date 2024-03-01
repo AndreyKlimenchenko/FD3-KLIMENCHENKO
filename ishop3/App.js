655ddb848f4043d232a11be03dc1d3063144426a
AndreyKlimenchenko <klimenchenkoandrey@gmail.com>
2024-03-01 18:23:02 +0300

    added project

---
+++ ishop3/App.js
@@ -0,0 +1,13 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+import ShopBlock from "./components/Shop";
+
+import products from "./products.json";
+
+let shopName = "Onliner";
+
+ReactDOM.render(
+  <ShopBlock name={shopName} items={products} />,
+  document.getElementById("container")
+);
---
+++ ishop3/components/IshopInfo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./ItemInfo.css";
+
+class IshopInfo extends React.Component {
+  static displayName = "IshopInfo";
+
+  static propTypes = {
+    mode: PropTypes.number.isRequired,
+    item: PropTypes.object.isRequired,
+  };
+
+  render() {
+    return (
+      <div className="itemInfo" hidden={this.props.mode !== 1}>
+        <h3>{this.props.item ? this.props.item.name : null}</h3>
+        <a href={this.props.item ? this.props.item.url : null}>
+          {this.props.item ? this.props.item.url : null}
+        </a>
+        <span className="Price">
+          {this.props.item ? this.props.item.price : null}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default IshopInfo;
