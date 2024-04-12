527560a20c405c2d639d3f42b41c3bc252729658
AndreyKlimenchenko <klimenchenkoandrey@gmail.com>
2024-04-12 12:50:08 +0300

    added movie details styles

--- FinalProject/movies/src/components/MovieDetails/Index.js
+++ FinalProject/movies/src/components/MovieDetails/Index.js
@@ -6,12 +6,28 @@ function MovieDetails() {
   const params = useParams();
 
   const movie = useSelector((state) => {
-    const data = state.movies.data.find((element) => element.id.toString() === params.id);
-    return data
+    const data = state.movies.data.find(
+      (element) => element.id.toString() === params.id
+    );
+    return data;
   });
   console.log(movie);
   if (!movie) return null;
-  return <div className="container">{movie.title}</div>;
+  return (
+    <div className="container">
+      <div className="titleDateContainer">
+        <span className="movieTitle"> {movie.title}</span>
+        <span className="movieDescription"> {movie.release_date}</span>
+      </div>
+      <div
+        className="moviePoster"
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.poster_path})`,
+        }}
+      />
+      <div className="movieDescription">{movie.overview}</div>
+    </div>
+  );
 }
 
 export default MovieDetails;
